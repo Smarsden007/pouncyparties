b171bedc9f173ead8a1053ca092d926d880172e1
Smarsden007 <seannmarsden@gmail.com>
2022-07-20 18:49:00 -0700

    wip

--- src/components/ThreeList.js
+++ src/components/ThreeList.js
@@ -11,12 +11,12 @@ export default function ThreeList(props) {
                     <h1 className='mobileline'>Free Delivery</h1>
                     <p className='descsan'>We proudly offer free delivery and set-up in Sonoma, Marin and Napa counties, additional charges apply outside of these area's. We truly provide a hassle free experience from start to finish!</p>
                 </div>
-                <div className='line' />
+                
                 <div className='box2'>
                     <h1 className='mobileline'>Disinfecting</h1>
                     <p className='descsan'>We use an electrostatic sprayer between each use with peroxide cleaner to ensure all bouncers are free of germs and ready for your event!</p>
                 </div>
-                <div className='line' />
+               
                 <div className='box3'>
                     <h1 className='mobileline'>Commitment</h1>
                     <p className='descsan'>All of our employees are vacinated and are taking extra precautions to limit the spread of Covid-19 and provide you a safe experience.</p>
