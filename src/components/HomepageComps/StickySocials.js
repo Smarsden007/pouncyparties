84f96950002b6cf5dcb8fcff034a87ea14111856
Smarsden007 <seannmarsden@gmail.com>
2022-08-07 15:20:01 -0700

    changed target name for security

--- src/components/HomepageComps/StickySocials.js
+++ src/components/HomepageComps/StickySocials.js
@@ -11,7 +11,7 @@ export default function StickySocials(props) {
 
   return (
     <section className="icon-bar">
-      <a className='face'  target="_blank" href="https://www.theknot.com/marketplace/redirect-2062398?utm_source=vendor_website&utm_medium=banner&utm_term=36d0d760-d7ce-480a-9fff-2fb982745c18&utm_campaign=vendor_badge_assets"><img  alt="TheKnot.com" src="https://www.xoedge.com/myaccount/2019/grab-a-badge/social/flat/large/round.png" className='face' width='45' /></a>
+      <a className='face'  target="_theknot" href="https://www.theknot.com/marketplace/redirect-2062398?utm_source=vendor_website&utm_medium=banner&utm_term=36d0d760-d7ce-480a-9fff-2fb982745c18&utm_campaign=vendor_badge_assets"><img  alt="TheKnot.com" src="https://www.xoedge.com/myaccount/2019/grab-a-badge/social/flat/large/round.png" className='face' width='45' /></a>
       <a href="https://www.facebook.com/pouncyparties" className="face"><FaFacebook className="socials" /></a>
       <a href="https://www.tiktok.com/@pouncyparties?source=h5_m&_r=1" className="face">
         <FaTiktok className="socials" /></a>
