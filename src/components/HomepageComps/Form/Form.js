7aa4333916b7235e39182ff8ad98b93278368310
Smarsden007 <seannmarsden@gmail.com>
2022-08-16 20:59:54 -0700

    Working on making button disable until filled out

--- src/components/HomepageComps/Form/Form.js
+++ src/components/HomepageComps/Form/Form.js
@@ -1,156 +1,128 @@
 import React from 'react';
-import './form.css'
-
-
-
 import { useForm, ValidationError } from '@formspree/react';
 
+import {
+    FormMaster,
+    FormCol,
+    FormButton,
+    FormBox,
+    LabelRow,
+    FormLabel,
+    FormInput,
+    FormTextArea,
+    SelectForm,
+    FormOption
+} from './FormStyles'
 
 function ContactForm() {
-    const [state, handleSubmit] = useForm("xqknoaaq");
+    const [state, handleSubmit] = useForm("xzbokyra");
     if (state.succeeded) {
-        return <p className='return-message'>WE CAN'T WAIT TO CELEBRATE WITH YOU!</p>;
+        return ;
     }
-    return (
 
-        <section className='book-now1'>
-            <div className='background2'>
-               
-               
+
+    return (
+       
+            <FormMaster>
 
                 <form onSubmit={handleSubmit}>
+                    <FormCol>
+                        <FormBox>
+                            <LabelRow>
+                                <FormLabel htmlFor="email">
+                                    Name
+                                </FormLabel>
+                                <FormInput
+                                    id="name"
+                                    type="name"
+                                    name="name"
+                                />
+                                <ValidationError
+                                    prefix="Email"
+                                    field="email"
+                                    errors={state.errors}
+                                />
+                            </LabelRow>
+                            <LabelRow>
+                                <FormLabel htmlFor="email">
+                                    Company Name
+                                </FormLabel>
+                                <FormInput
+                                    id="name"
+                                    type="name"
+                                    name="name"
+                                />
+                                <ValidationError
+                                    prefix="Email"
+                                    field="email"
+                                    errors={state.errors}
+                                />
+                            </LabelRow>
+                            <LabelRow>
+                                <FormLabel htmlFor="email">
+                                    Email Address
+                                </FormLabel>
+                                <FormInput
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                />
+                                <ValidationError
+                                    prefix="Email"
+                                    field="email"
+                                    errors={state.errors}
+                                />
+                            </LabelRow>
+                            <LabelRow>
+                                <FormLabel htmlFor="email">
+                                    Phone
+                                </FormLabel>
+                                <FormInput
+                                    id="phone"
+                                    type="phone"
+                                    name="phone"
+                                />
+                                <ValidationError
+                                    prefix="phone"
+                                    field="phone"
+                                    errors={state.errors}
+                                />
+                            </LabelRow>
+                            <LabelRow> <FormLabel htmlFor="package">
+                                    Package
+                                </FormLabel>
+                                <SelectForm name="Packages" id='Packages'>
+                                    <FormOption value='smallempire'>The Toddler</FormOption>
+                                    <FormOption  value='limitedpackage'>The Classic Castle</FormOption >
+                                    <FormOption  value='premiumpackage'>The Basteel</FormOption >
+                                    <FormOption  value='empirepackage'>The Mix & Pounce</FormOption >
 
-                    <div className='master-form'>
-                        <div className='email1'>
-                            <label htmlFor="name">
-                                *first & last name
-                            </label>
-                            <input
-                                id="name"
-                                type="name"
-                                name="name"
-                                placeholder='Pouncy'
-                            />
-                        </div>
-
-
-                        <div className='email1'>
-                            <label htmlFor="phone">
-                                *phone
-                            </label>
-                            <input
-                                id="phone"
-                                type="phone"
-                                name="phone"
-                                placeholder='111-111-1111'
-                            />
-                        </div>
-                        <div className='email1'>
-                            <label htmlFor="email">
-                                *email
-                            </label>
-                            <input
-                                id="email"
-                                type="email"
-                                name="email"
-                                placeholder='hello@pouncyparties.com'
-                            />
-                        </div>
-                        <div className='bouncer'>
-                            <label htmlFor="bouncer">
-                                *bouncer
-                            </label>
-                            <select name="bouncetype" id="bouncetype">
-                                <option value="cass">The Classic Castle</option>
-                                <option value="mixx">The Pounce and Slide Mix</option>
-                                <option value="toddler">The Toddler Sized</option>
-                            </select>
-                        </div>
-                        <div className='city1'>
-                            <label htmlFor="eventaddy">
-                                *event address
-                            </label>
-                            <input
-                                id="address"
-                                type="address"
-                                name="address"
-                                placeholder='111 Pouncy Lane, Pouncy, California'
-                            />
-                        </div>
-                        <div className='city1'>
-                            <label htmlFor="eventstart">
-                                *event start time
-                            </label>
-                            <input
-                                id="time"
-                                type="time"
-                                name="date"
-                            />
-                        </div>
-                        <div className='city1'>
-                            <label htmlFor="eventstart">
-                                *event date
-                            </label>
-                            <input
-                                id="date"
-                                type="date"
-                                name="date"
-                            />
-                        </div>
-                         <div className='bouncer'>
-                            <label htmlFor="bouncer">
-                                *length of rental
-                            </label>
-                            <select name="bouncetype" id="bouncetype">
-                                <option value="cass">4-hrs</option>
-                                <option value="mixx">6hrs</option>
-                                <option value="toddler">8hrs</option>
-                            </select>
-                        </div>
-                        <label htmlFor="city">
-                            *what type of surface will the rental be set-up on?(we do not set-up on rock, dirt, or gravel)
-                        </label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            placeholder='Grass, Dirt, Pavement etc. (WE DO NOT SET-UP ON GRAVEL)'
-                        />
-                        <label htmlFor="city">
-                            *how did you hear about us?
-                        </label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            placeholder='Facebook, Instagram, Twitter, TikTok'
-                        />
-                        <label htmlFor="city">
-                            coupon code
-                        </label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            placeholder='LETSGETPOUNCY'
-                        />
-
-                        <ValidationError
-                            prefix="Email"
-                            field="email" 
-                            errors={state.errors}
-                        />
-
-
-                        <ValidationError
-                            prefix="message"
-                            field="message"
-                            errors={state.errors}
-                        />
-                        <button className='formb' type="submit" disabled={state.submitting}>
-                            SUBMIT
-                        </button>
-                    </div>
+                                </SelectForm>
+                            </LabelRow>
+                            <LabelRow>
+                            <FormLabel htmlFor="email">
+                                    Message
+                                </FormLabel>
+                                <FormTextArea
+                                    id="message"
+                                    name="message"
+                                />
+                                <ValidationError
+                                    prefix="Message"
+                                    field="message"
+                                    errors={state.errors}
+                                />
+                            </LabelRow>
+                            <FormButton type="submit" disabled={state.submitting}>
+                                Submit
+                            </FormButton>
+                        </FormBox>
+                    </FormCol>
                 </form>
-            </div>
-        </section>
+
+            </FormMaster>
+
+     
     );
 }
 function App() {
---
+++ src/components/HomepageComps/Form/FormStyles.js
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+</style>
+
+export const FormMaster = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+border-radius: 0.25rem;
+
+`
+export const FormCol = styled.div`
+display: flex;
+flex-direction: column;
+
+margin: 3rem;
+border-radius: 0.25rem;
+box-shadow: 0 7px 13px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`
+export const FormBox = styled.div`
+background-color: #E6BEAE;
+padding: 3rem;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: flex-end;
+border-radius: 0.25rem;
+
+`
+export const FormButton = styled.button`
+width: 14vw;
+height: 4vh;
+border: none;
+background-color: #C84C09;
+color: white;
+font-size: 1.5rem;
+font-family: 'Space Mono', monospace;
+margin: 1rem;
+&:hover{
+background-color: white;
+color: #333333;
+cursor: pointer;
+}
+@media screen and (max-width: 768px) {
+	font-size: 1rem;
+	}
+`
+export const LabelRow = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+color: white;
+padding: 1rem;
+`
+export const FormLabel = styled.label`
+padding-right: .75rem;
+font-size: 1.5rem;
+font-family: 'Space Mono', monospace;
+outline: #333333;
+@media screen and (max-width: 768px) {
+	font-size: 1rem;
+	}
+`
+export const FormInput = styled.input`
+width: 20vw;
+height: 4vh;
+font-size: 1.5rem;
+border: none;
+outline-color: #C84C09;
+padding: .55rem;
+@media screen and (max-width: 768px) {
+	height: 2vh;
+	font-size: 1rem;
+	}
+`
+export const FormTextArea = styled.textarea`
+width: 20vw;
+height: 10vh;
+padding: .55rem;
+outline-color: #C84C09;
+@media screen and (max-width: 768px) {
+	height: 5vh;
+
+}
+`
+export const SelectForm = styled.select`
+outline-color: #C84C09;
+width: 21.5vw;
+height: 5vh;
+@media screen and (max-width: 768px) {
+
+font-size: .75rem;
+}
+`
+export const FormOption = styled.option`
+background-color: #C84c09;
+&:hover {
+	background-color: #C84c09;
+}
+@media screen and (max-width: 768px) {
+
+	font-size: .5rem;
+	}
+`
